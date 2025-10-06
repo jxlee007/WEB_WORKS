5da32e8855dbef023c3b47821c1e4b51f16500b9
jxlee007 <jimmyfalcon@gmail.com>
2025-10-07 03:00:27 +0530

    reached till prints

---
+++ ASTRO/src/pages/shop/products.js
@@ -0,0 +1,129 @@
+export const products = [
+	{
+		slug: "photo-preset",
+		title: "lightroom presets",
+		subtitle: "custom preset pack for adobe lightroom cc.",
+		image: "/images/shop/shop-1.jpg",
+		href: "/shop/photo-preset",
+		layout: {
+			hero: "/images/shop/shop-2.jpg",
+			products: [
+				{
+					image: "/images/photo-lr-presets/2021v4.jpg",
+					title: "PM Lightroom V4 Presets 2021",
+				},
+				{
+					image: "/images/photo-lr-presets/2019v3.jpg",
+					title: "PM Lightroom V3 Presets 2019",
+				},
+				{
+					image: "/images/photo-lr-presets/2018fall.jpg",
+					title: "PM Lightroom V2 Presets 2018",
+				},
+				{
+					image: "/images/photo-lr-presets/2017v1.jpg",
+					title: "PM Lightroom V1 Presets 2017",
+				},
+			],
+		},
+	},
+	{
+		slug: "prints",
+		title: "pm prints",
+		subtitle: "limited edition archival prints.",
+		image: "/images/shop/shop-2.jpg",
+		href: "/prints",
+	},
+	{
+		slug: "motion-gfx",
+		title: "motion gfx",
+		subtitle: "professional titles / graphics / effects",
+		image: "/images/shop/shop-3.jpg",
+		href: "/shop/motion-gfx",
+		layout: {
+			hero: "/images/motion-fx/gfx-2.png",
+			products: [
+				{
+					image: "/images/motion-fx/scratchbox.jpg",
+					title: "Film Scratches FX",
+				},
+				{
+					image: "/images/motion-fx/glitchbox.jpg",
+					title: "Glitch FX",
+				},
+				{
+					image: "/images/motion-fx/mattebox.jpg",
+					title: "Film Mattes FX",
+				},
+				{
+					image: "/images/motion-fx/leaksbox.jpg",
+					title: "Light Leaks FX",
+				},
+			],
+		},
+	},
+	{
+		slug: "luts",
+		title: "video luts",
+		subtitle: "professional color grading for premiere / ae / fcpx / resolve",
+		image: "/images/shop/shop-4.jpg",
+		href: "/shop/luts",
+		layout: {
+			hero: "/images/collabs/collab-pg-2.jpg",
+			products: [
+				{
+					image: "/images/cineluts/cinelutsv1.jpg",
+					title: "Cine LUTS V1",
+				},
+				{
+					image: "/images/cineluts/cinelutsv2.jpg",
+					title: "Cine LUTS V2",
+				},
+			],
+		},
+	},
+	{
+		slug: "sound-packs",
+		title: "sound packs",
+		subtitle: "take your edits to the next level.",
+		image: "/images/shop/shop-5.jpg",
+		href: "/shop/sound-packs",
+		layout: {
+			hero: "/images/shop/shop-5.jpg",
+			products: [
+				{
+					image: "/images/pm-sound-pack-v1.jpg",
+					title: "Sound Pack V1",
+				},
+			],
+		},
+	},
+	{
+		slug: "gear",
+		title: "pm gear",
+		subtitle: "custom gear for your camera bag.",
+		image: "/images/shop/shop-6.jpg",
+		href: "/shop/gear",
+		layout: {
+			hero: "/images/shop/shop-6.jpg",
+			products: [
+				{
+					image: "/images/gear/gear-1.jpg",
+					title: "PM Camera Tool",
+				},
+				{
+					image: "/images/gear/gear-2.jpg",
+					title: "Signature Lens IDs",
+				},
+			],
+		},
+	},
+	{
+		slug: "pirates-life",
+		title: "jack's pirate life",
+		subtitle: "limited edition drops - fly the flag.",
+		image: "/images/shop/shop-7.jpg",
+		href: "/shop/pirates-life",
+		target: "_blank",
+	},
+];
